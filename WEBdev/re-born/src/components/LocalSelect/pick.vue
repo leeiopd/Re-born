c5d688dc201ae5de2b432d4f5f4c383c7a1dda64
김승훈 <gogo12394@naver.com>
2019-10-14 17:36:21 +0900

    191014|localselectpage 제작중

---
+++ WEBdev/re-born/src/components/LocalSelect/pick.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+      <v-flex class="subpick1">
+      <h2>여기서는 현재 어느 지역인지를 써주죠</h2>
+      </v-flex>
+      <v-flex class="subpick2">
+    <h2>여기는 세부 지역을 고를겁니다</h2>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.subpick1 {
+    border: 3px ridge;
+    height: 15%;
+}
+.subpick2 {
+    border: 3px ridge;
+    height: 85%;
+}
+</style>
