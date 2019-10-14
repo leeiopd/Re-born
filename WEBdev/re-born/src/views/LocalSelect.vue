b0476b134ca2bb068aab934b872392630c334fb9
김승훈 <gogo12394@naver.com>
2019-10-14 17:36:21 +0900

    191014|localselectpage 제작중

--- WEBdev/re-born/src/views/LocalSelect.vue
+++ WEBdev/re-born/src/views/LocalSelect.vue
@@ -1,10 +1,37 @@
 <template>
-  <h1>지역 선택</h1>
+  <v-container>
+    <h1>지역 선택</h1>
+    <v-flex row wrap>
+      <tiger class="tiger" />
+      <pick class="pick" />
+    </v-flex>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import tiger from "../components/LocalSelect/tiger";
+import pick from "../components/LocalSelect/pick";
+
+export default {
+  components: {
+    tiger,
+    pick
+  },
+};
 </script>
 
 <style>
+.tiger {
+  width: 57%;
+  border: 3px ridge;
+  height: 66vh;
+  margin-left: 2%;
+  margin-right: 1%;
+}
+.pick {
+  width: 38%;
+  border: 3px ridge;
+  margin-left: 1%;
+  margin-right: 1%;
+}
 </style>
