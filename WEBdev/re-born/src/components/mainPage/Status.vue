6c6f396a225820e0cd7444cd88d1b1e9f29eea4e
leeiopd <leeiopd@hanmail.net>
2019-10-16 13:47:24 +0900

    base bottomNav
    - bottom nav 기능 완성

---
+++ WEBdev/re-born/src/components/mainPage/Status.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="height: 100%; background-color: red; padding-top:3vw;">
+    <div class="container" style="margin:auto; width: 100%;">
+      <img src="../../assets/light-green.png" style="width: 40vw" />
+    </div>
+    <div>
+      <h1 style="font-size: 3vw; text-aling: center">오늘의 재활용은 좋음 입니다.</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
