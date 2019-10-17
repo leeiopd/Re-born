65357ea2da21acfb89942257cca3247dd27d26ba
leeiopd <leeiopd@hanmail.net>
2019-10-17 13:41:10 +0900

    homePage 구성 완료
    checkout
    
    checkout
    
    homePage 구성 완료
    
    - 재활용 정보 배치
    - 그래프 배치

---
+++ WEBdev/re-born/src/components/mainPage/Chart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container" style="padding-top: 5vh">
+    <v-row no-gutters style="height: 100%;">
+      <v-col cols="4">
+        <chartCan></chartCan>
+        <chartPaper></chartPaper>
+      </v-col>
+      <v-col cols="4">
+        <chartAll></chartAll>
+      </v-col>
+      <v-col cols="4">
+        <chartPET></chartPET>
+        <chartOther></chartOther>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import chartCan from "./chart/chartCan.vue";
+import chartPET from "./chart/chartPET.vue";
+import chartPaper from "./chart/chartPaper.vue";
+import chartOther from "./chart/chartOther.vue";
+import chartAll from "./chart/chartAll.vue";
+
+export default {
+  components: {
+    chartCan,
+    chartPET,
+    chartPaper,
+    chartOther,
+    chartAll
+  }
+};
+</script>
+
+<style>
+</style>
