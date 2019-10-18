9926077f826deb966f8d5edeb06b16c390d68d00
leeiopd <leeiopd@hanmail.net>
2019-10-18 15:28:35 +0900

    infoPage 구성 완료
    - 페이지 구성
    - 모달 버튼 완료

---
+++ WEBdev/re-born/src/components/infoPage/infoPET.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="1200">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="#033E8C"
+          dark
+          v-on="on"
+          @click="dialog = true"
+          fab
+          style="width: 11vw; height: 11vw; font-size: 3vw; font-family: 'Gamja Flower', cursive;"
+        >페트병</v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>Privacy Policy</v-card-title>
+
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false">I accept</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
