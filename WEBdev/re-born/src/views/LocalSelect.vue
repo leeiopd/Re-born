4f38879af9011c4afe92da3f8742194951302b55
leeiopd <leeiopd@hanmail.net>
2019-10-21 16:02:51 +0900

    rebase
    Former-commit-id: b151ec1a933362e1d769a99498bfeaf77937ec4d

--- WEBdev/re-born/src/views/LocalSelect.vue
+++ WEBdev/re-born/src/views/LocalSelect.vue
@@ -2,36 +2,20 @@
   <v-container>
     <h1>지역 선택</h1>
     <v-flex row wrap>
-      <tiger class="tiger" />
-      <pick class="pick" />
+      <kakaomap />
     </v-flex>
   </v-container>
 </template>
 
 <script>
-import tiger from "../components/LocalSelect/tiger";
-import pick from "../components/LocalSelect/pick";
+import kakaomap from "../components/LocalSelect/kakaomap";
 
 export default {
   components: {
-    tiger,
-    pick
+    kakaomap
   },
 };
 </script>
 
 <style>
-.tiger {
-  width: 57%;
-  border: 3px ridge;
-  height: 66vh;
-  margin-left: 2%;
-  margin-right: 1%;
-}
-.pick {
-  width: 38%;
-  border: 3px ridge;
-  margin-left: 1%;
-  margin-right: 1%;
-}
 </style>
