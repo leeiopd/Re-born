6b7ed298dea9a5a9b5fde746a15de17fb9863767
YonungWoo <lkkjasd@naver.com>
2019-10-21 15:03:57 +0900

    191021|REST_API SERVER
    - ERD 작성
    - Class를 LocalLevel 1, 2, 3 및 Place로 나누어 세부 내용작성

--- APISERVERdev/RebornAPI/local/admin.py
+++ APISERVERdev/RebornAPI/local/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from local.models import City, Gun, Gu, Dong
+from local.models import LocalLevel3, LocalLevel1, LocalLevel2, Place
 # Register your models here.
 
-admin.site.register(City)
-admin.site.register(Gun)
-admin.site.register(Gu)
-admin.site.register(Dong)
+admin.site.register(LocalLevel1)
+admin.site.register(LocalLevel2)
+admin.site.register(LocalLevel3)
+admin.site.register(Place)
