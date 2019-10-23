bee5c32cd0f86247c4ab2fd9cf7bf8ac433a5e0b
leeiopd <leeiopd@hanmail.net>
2019-10-23 17:07:45 +0900

    mainPage api연동
    Former-commit-id: ee6dfd80c0cb29b49da97ac07b289d3b8cd51573

--- APISERVERdev/RebornAPI/local/admin.py
+++ APISERVERdev/RebornAPI/local/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from local.models import SpecialSi, Do, Si, Gu, Dong, Place
 # Register your models here.
 
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
 admin.site.register(SpecialSi)
 admin.site.register(Do)
 admin.site.register(Si)
 admin.site.register(Gu)
 admin.site.register(Dong)
-admin.site.register(Place)
+admin.site.register(Place, PlaceAdmin)
