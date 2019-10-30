be29d691a59242345e24572b6ae68e0f84c60b78
김승훈 <gogo12394@naver.com>
2019-10-30 13:53:38 +0900

    191030|localselect page add modal
    Former-commit-id: a90d297a6d74d045173247b55f86d90083e7110a

--- WEBdev/re-born/src/components/LocalSelect/select.vue
+++ WEBdev/re-born/src/components/LocalSelect/select.vue
@@ -1,110 +1,139 @@
 <template>
-  <div style="width: 80vw; height: 100%; text-align:center;">
-    <h1>{{addressLevel1}} {{addressLevel2}} {{addressLevel3}} {{addressPlace}}</h1>
-    <v-row>
-      <v-col style="width: 20%;">
-        <div>
-          <v-btn
-            v-on:click="level1ItemsPrev(level1ItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-up"></i>
-          </v-btn>
-          <v-list v-for="(item, i) in level1Items" :key="i" style="padding: 0;margin: 2vh;">
+  <v-container>
+    <div style="width: 80vw; height: 100%; text-align:center;">
+      <h1>{{addressLevel1}} {{addressLevel2}} {{addressLevel3}} {{addressPlace}}</h1>
+      <v-row>
+        <v-col style="width: 20%;">
+          <div>
             <v-btn
-              v-on:click="itemSelectLevel1(item.name, item.pk)"
-              outlined
-              color="indigo"
-              style="width: 100%; font-size: 3vh"
-            >{{item.name}}</v-btn>
-          </v-list>
-          <v-btn
-            v-on:click="level1ItemsNext(level1ItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-down"></i>
-          </v-btn>
-        </div>
-      </v-col>
-      <v-col style="width: 20%;">
-        <div>
-          <v-btn
-            v-on:click="level2ItemsPrev(level2ItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-up"></i>
-          </v-btn>
-          <v-list v-for="(item, i) in level2Items" :key="i" style="padding: 0;margin: 2vh;">
+              v-on:click="level1ItemsPrev(level1ItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-up"></i>
+            </v-btn>
+            <v-list v-for="(item, i) in level1Items" :key="i" style="padding: 0;margin: 2vh;">
+              <v-btn
+                v-on:click="itemSelectLevel1(item.name, item.pk)"
+                outlined
+                color="indigo"
+                style="width: 100%; font-size: 3vh"
+              >{{item.name}}</v-btn>
+            </v-list>
             <v-btn
-              v-on:click="itemSelectLevel2(item.name, item.pk)"
-              outlined
-              color="indigo"
-              style="width: 100%; font-size: 3vh;"
-            >{{item.name}}</v-btn>
-          </v-list>
-          <v-btn
-            v-on:click="level2ItemsNext(level2ItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-down"></i>
-          </v-btn>
-        </div>
-      </v-col>
-      <v-col style="width: 20%;">
-        <div>
-          <v-btn
-            v-on:click="level3ItemsPrev(level3ItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-up"></i>
-          </v-btn>
-          <v-list v-for="(item, i) in level3Items" :key="i" style="padding: 0;margin: 2vh;">
+              v-on:click="level1ItemsNext(level1ItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-down"></i>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col style="width: 20%;">
+          <div>
             <v-btn
-              v-on:click="itemSelectLevel3(item.name, item.pk)"
-              outlined
-              color="indigo"
-              style="width: 100%; font-size: 3vh"
-            >{{item.name}}</v-btn>
-          </v-list>
-          <v-btn
-            v-on:click="level3ItemsNext(level3ItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-down"></i>
-          </v-btn>
-        </div>
-      </v-col>
-      <v-col style="width: 20%;">
-        <div>
-          <v-btn
-            v-on:click="placeItemsPrev(placeItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-up"></i>
-          </v-btn>
-          <v-list v-for="(item, i) in placeItems" :key="i" style="padding: 0;margin: 2vh;">
+              v-on:click="level2ItemsPrev(level2ItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-up"></i>
+            </v-btn>
+            <v-list v-for="(item, i) in level2Items" :key="i" style="padding: 0;margin: 2vh;">
+              <v-btn
+                v-on:click="itemSelectLevel2(item.name, item.pk)"
+                outlined
+                color="indigo"
+                style="width: 100%; font-size: 3vh;"
+              >{{item.name}}</v-btn>
+            </v-list>
             <v-btn
-              v-on:click="itemSelectPalce(item.pk, item.name)"
-              outlined
-              color="indigo"
-              style="width: 100%; font-size: 3vh"
-            >{{item.name}}</v-btn>
-          </v-list>
-          <v-btn
-            v-on:click="placeItemsNext(placeItemsStart)"
-            style="font-size:2vw; width: 1vw; height: 2vw; "
-          >
-            <i class="fas fa-angle-double-down"></i>
-          </v-btn>
-        </div>
-      </v-col>
-    </v-row>
-  </div>
+              v-on:click="level2ItemsNext(level2ItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-down"></i>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col style="width: 20%;">
+          <div>
+            <v-btn
+              v-on:click="level3ItemsPrev(level3ItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-up"></i>
+            </v-btn>
+            <v-list v-for="(item, i) in level3Items" :key="i" style="padding: 0;margin: 2vh;">
+              <v-btn
+                v-on:click="itemSelectLevel3(item.name, item.pk)"
+                outlined
+                color="indigo"
+                style="width: 100%; font-size: 3vh"
+              >{{item.name}}</v-btn>
+            </v-list>
+            <v-btn
+              v-on:click="level3ItemsNext(level3ItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-down"></i>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col style="width: 20%;">
+          <div>
+            <v-btn
+              v-on:click="placeItemsPrev(placeItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-up"></i>
+            </v-btn>
+            <v-list v-for="(item, i) in placeItems" :key="i" style="padding: 0;margin: 2vh;">
+              <v-btn
+                v-on:click="itemSelectPalce(item.pk, item.name)"
+                outlined
+                color="indigo"
+                style="width: 100%; font-size: 3vh"
+              >{{item.name}}</v-btn>
+            </v-list>
+            <v-btn
+              v-on:click="placeItemsNext(placeItemsStart)"
+              style="font-size:2vw; width: 1vw; height: 2vw; "
+            >
+              <i class="fas fa-angle-double-down"></i>
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="scale-transition" >
+      <v-row no-gutters style="height: 100%;">
+        <v-col cols="6">
+          <v-card class="pa-2" outlined tile style="height: 100%; text-align: center;">
+            <v-btn absolute @click="dialog=false">이것은 닫기버튼입니다 배치를 어떻게...</v-btn>
+            <Status 
+              :stid="this.stid"
+              :placeName="this.stplaceName"
+            />
+          </v-card>
+        </v-col>
+        <v-col cols="6" style="height: 100%;">
+          <v-card class="pa-2" outlined tile style="height: 100%;">
+            <Chart 
+              :stid="this.stid"
+            />
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-dialog>
+  </v-container>
 </template>
 
 <script>
 import axios from "axios";
+import Status from "../mainPage/Status.vue";
+import Chart from "../mainPage/Chart.vue";
+
 export default {
+  components: {
+    Status,
+    Chart
+  },
   data: () => ({
     addressLevel1: "서울특별시",
     level1Items: [],
@@ -129,7 +158,10 @@ export default {
     placeItemSelect: "",
     placeItemPk: 1,
     placeLast: 0,
-    placeItemsStart: 0
+    placeItemsStart: 0,
+    dialog: false,
+    stid: 1,
+    stplaceName: '유성연수원점'
   }),
   created() {
     this.loadLevel1(this.level1ItemsStart);
@@ -201,20 +233,15 @@ export default {
       axios
         .get(`${baseURL}/api/placelist/${pk}/`)
         .then(result => {
-          // console.log(result);
-          console.log(result.data, "122222222222222211");
           if (result.data.length === 0) {
-            // console.log(this.placeItems, "11111111111111111111111111111");
             const temp = [[], [], [], [], []];
             temp[0] = { pk: 99999, name: "X" };
             temp[1] = { pk: 99999, name: "설치된 장소가" };
             temp[2] = { pk: 99999, name: "  없습니다.  " };
             temp[3] = { pk: 99999, name: "X" };
             temp[4] = { pk: 99999, name: "X" };
-
             this.placeItems = temp;
           } else {
-            console.log(result.data[0]);
             const temp = [[], [], [], [], []];
             const data = result.data[0];
             temp[0] = data;
@@ -223,7 +250,6 @@ export default {
             temp[3] = { pk: 99999, name: "X" };
             temp[4] = { pk: 99999, name: "X" };
             this.placeItems = temp;
-            console.log(this.placeItems);
           }
         })
         .catch(error => {
@@ -260,6 +286,9 @@ export default {
         alert("아직 설치되지 않은 지역입니다.");
       }
       this.placesItemSelect = city;
+      this.stid = pk;
+      this.stplaceName = city;
+      this.dialog = true;
     },
     level1ItemsNext(start) {
       if (start < this.level1Last) {
@@ -302,5 +331,15 @@ export default {
   }
 };
 </script>
+
 <style>
+.v-dialog--fullscreen {
+  border-radius: 0;
+  margin: 0;
+  height: 68vh;
+  position: fixed;
+  overflow-y: auto;
+  top: 22vh; 
+  left: 0;
+}
 </style>
