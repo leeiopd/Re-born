d8e034043aef24570fc8627d580b4798907b5e54
김승훈 <gogo12394@naver.com>
2019-10-30 09:50:38 +0900

    191030|local db 추가 및 model 추가
    Former-commit-id: f896fa7ba25e6e8d4295125c4cc2493a5085db1f

--- APISERVERdev/RebornAPI/local/admin.py
+++ APISERVERdev/RebornAPI/local/admin.py
@@ -6,10 +6,14 @@ class Level1Admin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
 class Level2Admin(admin.ModelAdmin):
-    list_display = ['id', 'name']
+    list_display = ['id', 'name', 'level1']
 
 class Level3Admin(admin.ModelAdmin):
-    list_display = ['id', 'name']
+    list_display = ['id', 'name', 'get_level1', 'level2']
+
+    def get_level1(self, obj):
+        # level2 = obj.level2.level1
+        return obj.level2.level1
 
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
