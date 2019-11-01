d95ce8c86283a0e5cb3bda6a9604ffa8726d4c3a
김승훈 <gogo12394@naver.com>
2019-11-01 13:54:57 +0900

    191101|닫기 버튼 추가
    Former-commit-id: ed84890905ca2fc7fd85c9e8906a6c75eb8d6d5f

--- WEBdev/re-born/src/components/infoPage/infoPaper.vue
+++ WEBdev/re-born/src/components/infoPage/infoPaper.vue
@@ -21,7 +21,7 @@
         <v-divider></v-divider>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false">I accept</v-btn>
+          <v-btn color="primary" text @click="dialog = false">닫기</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
